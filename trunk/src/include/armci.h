eeb9bbc03632852bb747e7b00ebf16536ebdbcb3
balaji <balaji@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-06-19 16:48:38 +0000

    Added a backward compatibility layer for ARMCI.
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@10 4a0c8edc-5189-0410-a5da-cd986011a6a8

---
+++ trunk/src/include/armci.h
@@ -0,0 +1,16 @@
+/* -*- Mode: C; c-basic-offset:4 ; -*- */
+/*
+ *  (C) 2010 by Argonne National Laboratory.
+ *      See COPYRIGHT in top-level directory.
+ */
+
+#if !defined ARMCI_H_INCLUDED
+#define ARMCI_H_INCLUDED
+
+#define ARMCI_SUCCESS A1_SUCCESS
+#define ARMCI_ERROR   A1_ERROR
+
+#define ARMCI_Init    A1_Init
+#define ARMCI_Finalie A1_Finalize
+
+#endif /* ARMCI_H_INCLUDED */
