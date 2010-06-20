621cdd344d9f7f3287a7b9210509a062b1ddae97
balaji <balaji@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-06-20 01:11:12 +0000

    Rename DCMF to DCMFD.
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@20 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/a1d/dcmf/Makefile.mk
+++
@@ -1,7 +0,0 @@
-#
-# Copyright (C) 2010 by Argonne National Laboratory.
-#     See COPYRIGHT in top-level directory.
-#
-
-liba1_la_SOURCES += $(top_srcdir)/src/a1d/dcmf/dcmf_init.c \
-	$(top_srcdir)/src/a1d/dcmf/dcmf_finalize.c
---
+++ trunk/src/a1d/dcmfd/Makefile.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2010 by Argonne National Laboratory.
+#     See COPYRIGHT in top-level directory.
+#
+
+liba1_la_SOURCES += $(top_srcdir)/src/a1d/dcmfd/dcmfd_init.c \
+	$(top_srcdir)/src/a1d/dcmfd/dcmfd_finalize.c
