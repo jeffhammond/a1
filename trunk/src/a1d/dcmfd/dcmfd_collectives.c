ba963eae7638222e05d08d6f042e92b7207ffb6e
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-05 20:49:58 +0000

    remove GlobalBarrier
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@85 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/a1d/dcmfd/dcmfd_collectives.c
+++ trunk/src/a1d/dcmfd/dcmfd_collectives.c
@@ -6,29 +6,7 @@
 
 #include "dcmfdimpl.h"
 
-void A1DI_GlobalBarrier()
-{
-    DCMF_Result result = DCMF_SUCCESS;
-    DCMF_Request_t request;
-
-    A1U_FUNC_ENTER();
-
-    A1D_GlobalBarrier_info.active=1;
-    result = DCMF_GlobalBarrier(&A1D_GlobalBarrier_info.protocol,
-                       &request,
-                       A1D_GlobalBarrier_info.callback);
-    A1U_ERR_ABORT(result,"Global Barrier failed \n");
-    while(A1D_GlobalBarrier_info.active) DCMF_Messager_advance();
-
-  fn_exit:
-    A1U_FUNC_EXIT();
-    return;
-
-  fn_fail:
-    goto fn_exit;
-}
-
-void A1D_Barrier(A1_group_t* group) { 
+void A1D_Barrier_group(A1_group_t* group) {
 
     A1U_FUNC_ENTER();
 
