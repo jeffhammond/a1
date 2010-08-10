b9666f524d4843ad127c17616607748853d6df17
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-10 12:00:22 +0000

    Jeff: never should have committed this file in the first place
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@134 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/a1d/dcmfd/dcmfd_cht.c
+++
@@ -1,9 +0,0 @@
-/* -*- Mode: C; c-basic-offset:4 ; -*- */
-/*
- *  Copyright (C) 2010 by Argonne National Laboratory.
- *      See COPYRIGHT in top-level directory.
- */
-
-#include "dcmfdimpl.h"
-
-
