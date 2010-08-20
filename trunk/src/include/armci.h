65af0f2610b07d1f87f74bdd4ea186c9b03309aa
potluri <potluri@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-20 22:21:55 +0000

    adding definitions for vector ops and armci wrappers
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@230 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/include/armci.h
+++ trunk/src/include/armci.h
@@ -4,8 +4,44 @@
  *      See COPYRIGHT in top-level directory.
  */
 
+#include a1.h
+
 #if !defined ARMCI_H_INCLUDED
 #define ARMCI_H_INCLUDED
 
+#define ARMCI_Init() \
+        A1_Initialize(A1_THREAD_SINGLE)
+
+#define ARMCI_Finalize() \
+        A1_Finalize()
+
+#define ARMCI_Put(src, dst, bytes, proc) \
+        A1_Put(proc, src, dst, bytes)
+
+#define ARMCI_PutS(src_ptr, src_stride_ar, dst_ptr, dst_stride_ar, \ 
+                   count, stride_levels, proc) \
+        A1_PutS(proc, stride_levels, count, src_ptr, src_stride_ar, \
+                   dst_ptr, dst_stride_ar)                           
+
+#define ARMCI_Get(src, dst, bytes, proc) \
+        A1_Get(proc, src, dst, bytes)
+
+#define ARMCI_GetS(src_ptr, src_stride_ar, dst_ptr, dst_stride_ar, \
+                   count, stride_levels, proc) \
+        A1_GetS(proc, stride_levels, count, src_ptr, src_stride_ar, \
+                   dst_ptr, dst_stride_ar)  
+
+#define ARMCI_Acc(datatype, scale, src, dst, \
+                  bytes, proc) \
+        A1_PutAcc(proc, src, dst, bytes, datatype \
+                  scale) \
+
+#define ARMCI_AccS(datatype, scale, src_ptr, src_stride_ar, dst_ptr, dst_stride_ar, \
+                   count, stride_levels, proc) \
+        A1_PutAccS(proc, stride_levels, count, src_ptr, src_stride_ar, \
+                   dst_ptr, dst_stride_ar, datatype, scale)  
+
+#define ARMCI_Barrier() \
+        A1_Barrier_group(A1_GROUP_WORLD)
 
 #endif /* ARMCI_H_INCLUDED */
