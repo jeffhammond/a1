b6d1030ea29e9abe80d63243be7ed0ba09766b30
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-20 15:39:47 +0000

    group calls need to have the if world else error or we're going to deadlock on groups.  of course, we don't have groups implemented yet, but we shouldn't let that stop us from being careful about the implementation now.
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@227 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/a1d/dcmfd/dcmfd_free.c
+++ trunk/src/a1d/dcmfd/dcmfd_free.c
@@ -14,7 +14,17 @@ int A1D_Release_segments(A1_group_t* group, void *ptr)
 
     A1DI_CRITICAL_ENTER();
 
-    A1DI_GlobalBarrier();
+    if (group == A1_GROUP_WORLD || group == NULL)
+    {
+        status = A1DI_GlobalBarrier();
+        A1U_ERR_ABORT(status != A1_SUCCESS, "DCMF_GlobalBarrier returned with an error");
+        goto fn_exit;
+    }
+    else
+    {
+        A1U_ERR_POP(1, "A1D_Release_segments not implemented for non-world groups!");
+        goto fn_fail;
+    }
 
     A1DI_Free(ptr);
 
