049127f8990be947da7ab406fc63df4ee19e69fc
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-22 15:53:45 +0000

    fix buildsystem problems
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@255 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/adl/api/Makefile.mk
+++ trunk/src/adl/api/Makefile.mk
@@ -3,22 +3,22 @@
 #     See COPYRIGHT in top-level directory.
 #
 
-liba1_la_SOURCES += $(top_srcdir)/src/adl/util/adl_initialize.c \
-	$(top_srcdir)/src/adl/util/adl_finalize.c \
-	$(top_srcdir)/src/adl/util/adl_malloc.c \
-	$(top_srcdir)/src/adl/util/adl_free.c \
-	$(top_srcdir)/src/adl/util/adl_flush.c \
-	$(top_srcdir)/src/adl/util/adl_flush_group.c \
-	$(top_srcdir)/src/adl/util/adl_put.c \
-	$(top_srcdir)/src/adl/util/adl_puts.c \
-	$(top_srcdir)/src/adl/util/adl_putv.c \
-	$(top_srcdir)/src/adl/util/adl_get.c \
-	$(top_srcdir)/src/adl/util/adl_gets.c \
-	$(top_srcdir)/src/adl/util/adl_getv.c \
-    $(top_srcdir)/src/adl/util/adl_putacc.c \
-    $(top_srcdir)/src/adl/util/adl_putaccs.c \
-    $(top_srcdir)/src/adl/util/adl_putaccv.c \
-    $(top_srcdir)/src/adl/util/adl_wait.c \
-	$(top_srcdir)/src/adl/util/adl_collectives.c \
-	$(top_srcdir)/src/adl/util/adl_misc.c \
-	$(top_srcdir)/src/adl/util/adl_handle.c
+liba1_la_SOURCES += $(top_srcdir)/src/adl/api/adl_initialize.c \
+	$(top_srcdir)/src/adl/api/adl_finalize.c \
+	$(top_srcdir)/src/adl/api/adl_malloc.c \
+	$(top_srcdir)/src/adl/api/adl_free.c \
+	$(top_srcdir)/src/adl/api/adl_flush.c \
+	$(top_srcdir)/src/adl/api/adl_flush_group.c \
+	$(top_srcdir)/src/adl/api/adl_put.c \
+	$(top_srcdir)/src/adl/api/adl_puts.c \
+	$(top_srcdir)/src/adl/api/adl_putv.c \
+	$(top_srcdir)/src/adl/api/adl_get.c \
+	$(top_srcdir)/src/adl/api/adl_gets.c \
+	$(top_srcdir)/src/adl/api/adl_getv.c \
+    $(top_srcdir)/src/adl/api/adl_putacc.c \
+    $(top_srcdir)/src/adl/api/adl_putaccs.c \
+    $(top_srcdir)/src/adl/api/adl_putaccv.c \
+    $(top_srcdir)/src/adl/api/adl_wait.c \
+	$(top_srcdir)/src/adl/api/adl_collectives.c \
+	$(top_srcdir)/src/adl/api/adl_misc.c \
+	$(top_srcdir)/src/adl/api/adl_handle.c
