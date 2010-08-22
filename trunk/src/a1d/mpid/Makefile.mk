deaa2e44e23e1a2b50841b7641ef75eb37a87eeb
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-22 17:18:57 +0000

    obvious fix
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@261 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/a1d/mpid/Makefile.mk
+++ trunk/src/a1d/mpid/Makefile.mk
@@ -3,14 +3,14 @@
 #     See COPYRIGHT in top-level directory.
 #
 
-liba1_la_SOURCES += $(top_srcdir)/src/a1d/mpi2rmad/mpi2rmad_param.c \
-        $(top_srcdir)/src/a1d/mpi2rmad/mpi2rmad_initialize.c \
-	$(top_srcdir)/src/a1d/mpi2rmad/mpi2rmad_finalize.c \
-	$(top_srcdir)/src/a1d/mpi2rmad/mpi2rmad_malloc.c \
-	$(top_srcdir)/src/a1d/mpi2rmad/mpi2rmad_free.c \
-	$(top_srcdir)/src/a1d/mpi2rmad/mpi2rmad_flush.c \
-	$(top_srcdir)/src/a1d/mpi2rmad/mpi2rmad_flush_all.c \
-	$(top_srcdir)/src/a1d/mpi2rmad/mpi2rmad_put.c \
-	$(top_srcdir)/src/a1d/mpi2rmad/mpi2rmad_get.c \
-	$(top_srcdir)/src/a1d/mpi2rmad/mpi2rmad_collectives.c \
-	$(top_srcdir)/src/a1d/mpi2rmad/mpi2rmad_misc.c 
+liba1_la_SOURCES += $(top_srcdir)/src/a1d/mpid/mpid_param.c \
+        $(top_srcdir)/src/a1d/mpid/mpid_initialize.c \
+	$(top_srcdir)/src/a1d/mpid/mpid_finalize.c \
+	$(top_srcdir)/src/a1d/mpid/mpid_malloc.c \
+	$(top_srcdir)/src/a1d/mpid/mpid_free.c \
+	$(top_srcdir)/src/a1d/mpid/mpid_flush.c \
+	$(top_srcdir)/src/a1d/mpid/mpid_flush_all.c \
+	$(top_srcdir)/src/a1d/mpid/mpid_put.c \
+	$(top_srcdir)/src/a1d/mpid/mpid_get.c \
+	$(top_srcdir)/src/a1d/mpid/mpid_collectives.c \
+	$(top_srcdir)/src/a1d/mpid/mpid_misc.c 
