d78d738924ab878b88bdfe2aa3008bb876e9d5a1
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-24 02:15:36 +0000

    implement list versions of wait and test. Sreeram: please check that I've done this properly.
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@281 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/a1d/dcmfd/dcmfd_wait.c
+++ trunk/src/a1d/dcmfd/dcmfd_wait.c
@@ -28,6 +28,36 @@ int A1D_Wait_handle(A1_handle_t a1_handle)
     goto fn_exit;
 }
 
+
+int A1D_Wait_handle_list(int count, A1_handle_t *a1_handle)
+{
+    int status = A1_SUCCESS;
+    int composite_active = A1_TRUE;
+    A1D_Handle_t *a1d_handle;
+
+    A1U_FUNC_ENTER();
+
+    A1DI_CRITICAL_ENTER();
+
+    /* TODO: verify that this is the correct implementation */
+    do {
+        A1DI_Advance();
+        for(i=0;i<count;i++)
+        {
+            a1d_handle = (A1D_Handle_t *) a1_handle[i];
+            composite_active = composite_active || ( (a1d_handle->active > 0) ? A1_FALSE : A1_TRUE );
+        }
+    } while(composite_active == A1_TRUE)
+
+  fn_exit:
+    A1DI_CRITICAL_EXIT();
+    A1U_FUNC_EXIT();
+    return status;
+
+  fn_fail:
+    goto fn_exit;
+}
+
 int A1D_Test_handle(A1_handle_t a1_handle, A1_bool_t* completed)
 {
     int status = A1_SUCCESS;
@@ -40,6 +70,34 @@ int A1D_Test_handle(A1_handle_t a1_handle, A1_bool_t* completed)
     a1d_handle = (A1D_Handle_t *) a1_handle;
     A1DI_Advance();
     *completed = (a1d_handle->active > 0) ? A1_FALSE : A1_TRUE;
+
+  fn_exit:
+    A1DI_CRITICAL_EXIT();
+    A1U_FUNC_EXIT();
+    return status;
+
+  fn_fail:
+    goto fn_exit;
+}
+
+
+int A1D_Test_handle_list(int count, A1_handle_t *a1_handle, A1_bool_t* *completed)
+{
+    int i;
+    int status = A1_SUCCESS;
+    A1D_Handle_t *a1d_handle;
+
+    A1U_FUNC_ENTER();
+
+    A1DI_CRITICAL_ENTER();
+
+    /* TODO: verify that this is the correct implementation */
+    A1DI_Advance();
+    for(i=0;i<count;i++)
+    {
+        a1d_handle = (A1D_Handle_t *) a1_handle[i];
+        *completed[i] = (a1d_handle->active > 0) ? A1_FALSE : A1_TRUE;
+    }
 
   fn_exit:
     A1DI_CRITICAL_EXIT();
--- trunk/src/adl/api/adl_wait.c
+++ trunk/src/adl/api/adl_wait.c
@@ -32,6 +32,30 @@ int A1_Wait_handle(A1_handle_t handle)
     goto fn_exit;
 }
 
+int A1_Wait_handle_list(int count, A1_handle_t *handle)
+{
+    int status = A1_SUCCESS;
+
+    A1U_FUNC_ENTER();
+
+    /* FIXME: The profiling interface needs to go here */
+
+    /* FIXME: Locking functionality needs to go here */
+
+#   ifdef HAVE_ERROR_CHECKING
+#   endif
+
+    status = A1D_Wait_handle_list(count, handle);
+    A1U_ERR_POP(status!=A1_SUCCESS, "A1D_Wait_handle_list returned an error\n");
+
+  fn_exit:
+    A1U_FUNC_EXIT();
+    return status;
+
+  fn_fail:
+    goto fn_exit;
+}
+
 int A1_Test_handle(A1_handle_t handle, A1_bool_t* completed)
 {
     int status = A1_SUCCESS;
@@ -47,6 +71,31 @@ int A1_Test_handle(A1_handle_t handle, A1_bool_t* completed)
 
     status = A1D_Test_handle(handle, completed);
     A1U_ERR_POP(status!=A1_SUCCESS, "A1D_Test_handle returned an error\n");
+
+  fn_exit:
+    A1U_FUNC_EXIT();
+    return status;
+
+  fn_fail:
+    goto fn_exit;
+}
+
+
+int A1_Test_handle_list(int count, A1_handle_t *handle, A1_bool_t* *completed)
+{
+    int status = A1_SUCCESS;
+
+    A1U_FUNC_ENTER();
+
+    /* FIXME: The profiling interface needs to go here */
+
+    /* FIXME: Locking functionality needs to go here */
+
+#   ifdef HAVE_ERROR_CHECKING
+#   endif
+
+    status = A1D_Test_handle_list(count, handle, completed);
+    A1U_ERR_POP(status!=A1_SUCCESS, "A1D_Test_handle_list returned an error\n");
 
   fn_exit:
     A1U_FUNC_EXIT();
