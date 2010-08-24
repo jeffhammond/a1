8fc8ca0e9ff5a288ebb353311cd7c46a191555f4
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-24 14:14:26 +0000

    add TODOs
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@285 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/a1d/dcmfd/dcmfd_wait.c
+++ trunk/src/a1d/dcmfd/dcmfd_wait.c
@@ -14,20 +14,18 @@ int A1D_Wait_handle_all()
     A1U_FUNC_ENTER();
 
     A1DI_CRITICAL_ENTER();
- 
-    for(index=0; index<a1_settings.handlepool_size; index++) 
+
+    for (index = 0; index < a1_settings.handlepool_size; index++)
     {
-        if(A1D_Active_handle_list[index] != NULL)
-             A1DI_Conditional_advance((A1D_Active_handle_list[index])->active > 0);
+        if (A1D_Active_handle_list[index] != NULL)
+            A1DI_Conditional_advance((A1D_Active_handle_list[index])->active > 0);
     }
 
-  fn_exit:
-    A1DI_CRITICAL_EXIT();
+    fn_exit: A1DI_CRITICAL_EXIT();
     A1U_FUNC_EXIT();
     return status;
 
-  fn_fail:
-    goto fn_exit;
+    fn_fail: goto fn_exit;
 }
 
 int A1D_Wait_handle(A1_handle_t a1_handle)
@@ -43,15 +41,12 @@ int A1D_Wait_handle(A1_handle_t a1_handle)
 
     A1DI_Conditional_advance(a1d_handle->active > 0);
 
-  fn_exit:
-    A1DI_CRITICAL_EXIT();
+    fn_exit: A1DI_CRITICAL_EXIT();
     A1U_FUNC_EXIT();
     return status;
 
-  fn_fail:
-    goto fn_exit;
+    fn_fail: goto fn_exit;
 }
-
 
 int A1D_Wait_handle_list(int count, A1_handle_t *a1_handle)
 {
@@ -63,19 +58,20 @@ int A1D_Wait_handle_list(int count, A1_handle_t *a1_handle)
 
     A1DI_CRITICAL_ENTER();
 
-    for(index=0; index<count; index++)
+    for (index = 0; index < count; index++)
     {
-       a1d_handle = (A1D_Handle_t *) a1_handle[index];
-       A1DI_Conditional_advance(a1d_handle->active > 0);
+        /* TODO: Do we really need to do a copy here?
+         *        Isn't it enough to pass the argument as const?
+         */
+        a1d_handle = (A1D_Handle_t *) a1_handle[index];
+        A1DI_Conditional_advance(a1d_handle->active > 0);
     }
 
-  fn_exit:
-    A1DI_CRITICAL_EXIT();
+    fn_exit: A1DI_CRITICAL_EXIT();
     A1U_FUNC_EXIT();
     return status;
 
-  fn_fail:
-    goto fn_exit;
+    fn_fail: goto fn_exit;
 }
 
 int A1D_Test_handle(A1_handle_t a1_handle, A1_bool_t* completed)
@@ -87,21 +83,23 @@ int A1D_Test_handle(A1_handle_t a1_handle, A1_bool_t* completed)
 
     A1DI_CRITICAL_ENTER();
 
-    a1d_handle = (A1D_Handle_t *) a1_handle;
     A1DI_Advance();
+    /* TODO: Do we really need to do a copy here?
+     *        Isn't it enough to pass the argument as const?
+     */
+    a1d_handle = (A1D_Handle_t *) a1_handle;
     *completed = (a1d_handle->active > 0) ? A1_FALSE : A1_TRUE;
 
-  fn_exit:
-    A1DI_CRITICAL_EXIT();
+    fn_exit: A1DI_CRITICAL_EXIT();
     A1U_FUNC_EXIT();
     return status;
 
-  fn_fail:
-    goto fn_exit;
+    fn_fail: goto fn_exit;
 }
 
-
-int A1D_Test_handle_list(int count, A1_handle_t *a1_handle, A1_bool_t* *completed)
+int A1D_Test_handle_list(int count,
+                         A1_handle_t *a1_handle,
+                         A1_bool_t* *completed)
 {
     int i;
     int status = A1_SUCCESS;
@@ -113,17 +111,18 @@ int A1D_Test_handle_list(int count, A1_handle_t *a1_handle, A1_bool_t* *complete
 
     /* TODO: verify that this is the correct implementation */
     A1DI_Advance();
-    for(i=0;i<count;i++)
+    for (i = 0; i < count; i++)
     {
+        /* TODO: Do we really need to do a copy here?
+         *        Isn't it enough to pass the argument as const?
+         */
         a1d_handle = (A1D_Handle_t *) a1_handle[i];
         *completed[i] = (a1d_handle->active > 0) ? A1_FALSE : A1_TRUE;
     }
 
-  fn_exit:
-    A1DI_CRITICAL_EXIT();
+    fn_exit: A1DI_CRITICAL_EXIT();
     A1U_FUNC_EXIT();
     return status;
 
-  fn_fail:
-    goto fn_exit;
+    fn_fail: goto fn_exit;
 }
