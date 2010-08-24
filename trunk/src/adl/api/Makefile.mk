51d00e836421b8b610411fc24837ad8492eabf93
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-24 15:04:54 +0000

    changing whitespace
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@289 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/adl/api/Makefile.mk
+++ trunk/src/adl/api/Makefile.mk
@@ -4,23 +4,23 @@
 #
 
 liba1_la_SOURCES += $(top_srcdir)/src/adl/api/adl_initialize.c \
-	$(top_srcdir)/src/adl/api/adl_finalize.c \
-	$(top_srcdir)/src/adl/api/adl_malloc.c \
-	$(top_srcdir)/src/adl/api/adl_free.c \
-	$(top_srcdir)/src/adl/api/adl_flush.c \
-	$(top_srcdir)/src/adl/api/adl_flush_group.c \
-	$(top_srcdir)/src/adl/api/adl_put.c \
-	$(top_srcdir)/src/adl/api/adl_puts.c \
-	$(top_srcdir)/src/adl/api/adl_putv.c \
-	$(top_srcdir)/src/adl/api/adl_get.c \
-	$(top_srcdir)/src/adl/api/adl_gets.c \
-	$(top_srcdir)/src/adl/api/adl_getv.c \
-        $(top_srcdir)/src/adl/api/adl_putacc.c \
-        $(top_srcdir)/src/adl/api/adl_putaccs.c \
-        $(top_srcdir)/src/adl/api/adl_putaccv.c \
-        $(top_srcdir)/src/adl/api/adl_rmw.c \
-        $(top_srcdir)/src/adl/api/adl_wait.c \
-	$(top_srcdir)/src/adl/api/adl_collectives.c \
-	$(top_srcdir)/src/adl/api/adl_misc.c \
-	$(top_srcdir)/src/adl/api/adl_handle.c \
-	$(top_srcdir)/src/adl/api/adl_armci.c
+                    $(top_srcdir)/src/adl/api/adl_finalize.c \
+                    $(top_srcdir)/src/adl/api/adl_malloc.c \
+                    $(top_srcdir)/src/adl/api/adl_free.c \
+                    $(top_srcdir)/src/adl/api/adl_flush.c \
+                    $(top_srcdir)/src/adl/api/adl_flush_group.c \
+                    $(top_srcdir)/src/adl/api/adl_put.c \
+                    $(top_srcdir)/src/adl/api/adl_puts.c \
+                    $(top_srcdir)/src/adl/api/adl_putv.c \
+                    $(top_srcdir)/src/adl/api/adl_get.c \
+                    $(top_srcdir)/src/adl/api/adl_gets.c \
+                    $(top_srcdir)/src/adl/api/adl_getv.c \
+                    $(top_srcdir)/src/adl/api/adl_putacc.c \
+                    $(top_srcdir)/src/adl/api/adl_putaccs.c \
+                    $(top_srcdir)/src/adl/api/adl_putaccv.c \
+                    $(top_srcdir)/src/adl/api/adl_rmw.c \
+                    $(top_srcdir)/src/adl/api/adl_wait.c \
+                    $(top_srcdir)/src/adl/api/adl_collectives.c \
+                    $(top_srcdir)/src/adl/api/adl_misc.c \
+                    $(top_srcdir)/src/adl/api/adl_handle.c \
+                    $(top_srcdir)/src/adl/api/adl_armci.c
