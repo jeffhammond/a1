7a0aed82efe7a6e766e8304a2cc501828fe3e41c
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-28 07:14:43 +0000

    Formatting only, because I think Sreeram likes the way Eclipse does it automagically.
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@316 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/benchmarks/a1/A1-vs-ARMCI/Get_A1.c
+++ trunk/benchmarks/a1/A1-vs-ARMCI/Get_A1.c
@@ -1,52 +1,52 @@
 /* 
-* The following is a notice of limited availability of the code, and disclaimer
-* which must be included in the prologue of the code and in all source listings
-* of the code.
-* 
-* Copyright (c) 2010  Argonne Leadership Computing Facility, Argonne National 
-* Laboratory
-* 
-* Permission is hereby granted to use, reproduce, prepare derivative works, and
-* to redistribute to others.
-* 
-* 
-*                          LICENSE
-* 
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are
-* met:
-* 
-* - Redistributions of source code must retain the above copyright
-*   notice, this list of conditions and the following disclaimer.
-* 
-* - Redistributions in binary form must reproduce the above copyright
-*   notice, this list of conditions and the following disclaimer listed
-*   in this license in the documentation and/or other materials
-*   provided with the distribution.
-* 
-* - Neither the name of the copyright holders nor the names of its
-*   contributors may be used to endorse or promote products derived from
-*   this software without specific prior written permission.
-* 
-* The copyright holders provide no reassurances that the source code
-* provided does not infringe any patent, copyright, or any other
-* intellectual property rights of third parties.  The copyright holders
-* disclaim any liability to any recipient for claims brought against
-* recipient by any third party for infringement of that parties
-* intellectual property rights.
-* 
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-* A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-* OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-* THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
-*/
+ * The following is a notice of limited availability of the code, and disclaimer
+ * which must be included in the prologue of the code and in all source listings
+ * of the code.
+ *
+ * Copyright (c) 2010  Argonne Leadership Computing Facility, Argonne National
+ * Laboratory
+ *
+ * Permission is hereby granted to use, reproduce, prepare derivative works, and
+ * to redistribute to others.
+ *
+ *
+ *                          LICENSE
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer listed
+ *   in this license in the documentation and/or other materials
+ *   provided with the distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * The copyright holders provide no reassurances that the source code
+ * provided does not infringe any patent, copyright, or any other
+ * intellectual property rights of third parties.  The copyright holders
+ * disclaim any liability to any recipient for claims brought against
+ * recipient by any third party for infringement of that parties
+ * intellectual property rights.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -56,72 +56,84 @@
 #define ITERATIONS 100
 #define SKIP 10
 
-int main() {
+int main()
+{
 
-   size_t i, rank, nranks, msgsize, dest;
-   long bufsize;
-   double **buffer;
-   double t_start, t_stop, t_latency;
-   
-   A1_Initialize(A1_THREAD_SINGLE); 
+    size_t i, rank, nranks, msgsize, dest;
+    long bufsize;
+    double **buffer;
+    double t_start, t_stop, t_latency;
 
-   rank = A1_Process_id(A1_GROUP_WORLD); 
-   nranks = A1_Process_total(A1_GROUP_WORLD);
+    A1_Initialize(A1_THREAD_SINGLE);
 
-   bufsize = MAX_MSG_SIZE*(ITERATIONS+SKIP);
-   buffer = (double **) malloc (sizeof(double *) * nranks);
-   A1_Alloc_segment(&(buffer[rank]), bufsize);
-   A1_Exchange_segments(A1_GROUP_WORLD, (void **) buffer); 
+    rank = A1_Process_id(A1_GROUP_WORLD);
+    nranks = A1_Process_total(A1_GROUP_WORLD);
 
-   for(i=0; i<bufsize/sizeof(double); i++) {
-     *(buffer[rank] + i) = 1.0 + rank;
-   }
+    bufsize = MAX_MSG_SIZE * (ITERATIONS + SKIP);
+    buffer = (double **) malloc(sizeof(double *) * nranks);
+    A1_Alloc_segment(&(buffer[rank]), bufsize);
+    A1_Exchange_segments(A1_GROUP_WORLD, (void **) buffer);
 
-   A1_Barrier_group(A1_GROUP_WORLD);
+    for (i = 0; i < bufsize / sizeof(double); i++)
+    {
+        *(buffer[rank] + i) = 1.0 + rank;
+    }
 
-   if(rank == 0) {
+    A1_Barrier_group(A1_GROUP_WORLD);
 
-     printf("A1_Get Latency in usec \n");
-     printf("%20s %22s \n", "Message Size", "Latency");
-     fflush(stdout);
+    if (rank == 0)
+    {
 
-     dest = 1;
-
-     for(msgsize=sizeof(double); msgsize<=MAX_MSG_SIZE; msgsize*=2) {
-
-        for(i=0; i<ITERATIONS+SKIP; i++) { 
-
-            if(i == SKIP)
-                t_start = A1_Time_seconds();              
-
-            A1_Get(1, (void *) ((size_t)buffer[dest] + (size_t)(i*msgsize)), (void *) ((size_t)buffer[rank] + (size_t)(i*msgsize)), msgsize); 
-
-        }
-        t_stop = A1_Time_seconds();
-        printf("%20d %20.2f \n", msgsize, ((t_stop-t_start)*1000000)/ITERATIONS);
+        printf("A1_Get Latency in usec \n");
+        printf("%20s %22s \n", "Message Size", "Latency");
         fflush(stdout);
 
-        for(i=0; i<(((ITERATIONS+SKIP)*msgsize)/sizeof(double)); i++) {
-           if(*(buffer[rank] + i) != (1.0 + dest)) {
-                 printf("Data validation failed At displacement : %d Expected : %f Actual : %f \n", 
-                             i, (1.0 + dest), *(buffer[rank] + i));
-                 fflush(stdout);
-                 return -1;
-           }
+        dest = 1;
+
+        for (msgsize = sizeof(double); msgsize <= MAX_MSG_SIZE; msgsize *= 2)
+        {
+
+            for (i = 0; i < ITERATIONS + SKIP; i++)
+            {
+
+                if (i == SKIP) t_start = A1_Time_seconds();
+
+                A1_Get(1, (void *) ((size_t) buffer[dest] + (size_t)(i
+                        * msgsize)), (void *) ((size_t) buffer[rank]
+                        + (size_t)(i * msgsize)), msgsize);
+
+            }
+            t_stop = A1_Time_seconds();
+            printf("%20d %20.2f \n", msgsize, ((t_stop - t_start) * 1000000)
+                    / ITERATIONS);
+            fflush(stdout);
+
+            for (i = 0; i < (((ITERATIONS + SKIP) * msgsize) / sizeof(double)); i++)
+            {
+                if (*(buffer[rank] + i) != (1.0 + dest))
+                {
+                    printf("Data validation failed At displacement : %d Expected : %f Actual : %f \n",
+                           i,
+                           (1.0 + dest),
+                           *(buffer[rank] + i));
+                    fflush(stdout);
+                    return -1;
+                }
+            }
+
+            for (i = 0; i < bufsize / sizeof(double); i++)
+            {
+                *(buffer[rank] + i) = 1.0 + rank;
+            }
         }
 
-        for(i=0; i<bufsize/sizeof(double); i++) {
-           *(buffer[rank] + i) = 1.0 + rank;
-        }
-     }
+    }
 
-   }
+    A1_Barrier_group(A1_GROUP_WORLD);
 
-   A1_Barrier_group(A1_GROUP_WORLD); 
+    A1_Release_segments(A1_GROUP_WORLD, buffer[rank]);
 
-   A1_Release_segments(A1_GROUP_WORLD, buffer[rank]); 
- 
-   A1_Finalize();
+    A1_Finalize();
 
-   return 0;
+    return 0;
 }
