87dc9e32243eb652d36d8da44dbe15429ee19907
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-29 23:02:56 +0000

    some API additions, small formatting changes, MPID notes updated
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@325 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/a1d/dcmfd/dcmfd_counter.c
+++ trunk/src/a1d/dcmfd/dcmfd_counter.c
@@ -15,8 +15,8 @@ volatile int counter_incr_active;
 long counter_incr_response;
 
 void A1DI_Counter_create_callback(void *clientdata,
-                                 const DCMF_Control_t *info,
-                                 size_t peer)
+                                  const DCMF_Control_t *info,
+                                  size_t peer)
 {
     memcpy((void *) &counter_create_response, (void *) info, sizeof(void *));
     counter_create_active--;
@@ -28,13 +28,14 @@ void A1DI_Counter_callback(void *clientdata,
 {
     int status = A1_SUCCESS;
     A1D_Counter_pkt_t *counter_pkt = (A1D_Counter_pkt_t *) info;
-     
-    if(counter_pkt->value_ptr == NULL) {
+
+    if (counter_pkt->value_ptr == NULL)
+    {
         /*This is a response packet*/
-        counter_incr_response = counter_pkt->value;       
-        counter_incr_active--; 
+        counter_incr_response = counter_pkt->value;
+        counter_incr_active--;
     }
-    else 
+    else
     {
         A1D_Counter_pkt_t response_pkt;
         DCMF_Control_t cmsg;
@@ -42,7 +43,7 @@ void A1DI_Counter_callback(void *clientdata,
 
         original = *(counter_pkt->value_ptr);
         *(counter_pkt->value_ptr) += response_pkt.value;
- 
+
         response_pkt.value_ptr = NULL;
         response_pkt.value = original;
 
@@ -53,12 +54,12 @@ void A1DI_Counter_callback(void *clientdata,
                               peer,
                               &cmsg);
         A1U_ERR_ABORT(status != DCMF_SUCCESS,
-                   "DCMF_Control failed in A1DI_Counter_callback\n");         
+                      "DCMF_Control failed in A1DI_Counter_callback\n");
     }
 }
 
 int A1DI_Counter_initialize()
-{   
+{
     int status = A1_SUCCESS;
     DCMF_Control_Configuration_t conf;
 
@@ -80,18 +81,16 @@ int A1DI_Counter_initialize()
     conf.network = DCMF_DEFAULT_NETWORK;
     conf.cb_recv = A1DI_Counter_callback;
     conf.cb_recv_clientdata = NULL;
-    
+
     status = DCMF_Control_register(&A1D_Counter_protocol, &conf);
     A1U_ERR_POP(status != DCMF_SUCCESS,
                 "Counter protocol registartion returned with error %d \n",
                 status);
 
-  fn_exit:
-    A1U_FUNC_EXIT(); 
+    fn_exit: A1U_FUNC_EXIT();
     return status;
 
-  fn_fail:
-    goto fn_exit;
+    fn_fail: goto fn_exit;
 }
 
 int A1D_Create_counter(A1_group_t* group, A1_counter_t *counter_ptr)
@@ -105,14 +104,14 @@ int A1D_Create_counter(A1_group_t* group, A1_counter_t *counter_ptr)
     A1DI_CRITICAL_ENTER();
 
     A1U_ERR_POP(status = (group != A1_GROUP_WORLD),
-            "Counters are not implemented for non-world groups!");
+                "Counters are not implemented for non-world groups!");
 
     status = A1DI_Malloc_aligned((void **) &a1d_counter, sizeof(A1D_Counter_t));
     A1U_ERR_POP(status != 0,
-                        "A1DI_Malloc_aligned returned error in A1D_Create_counter\n");
+                "A1DI_Malloc_aligned returned error in A1D_Create_counter\n");
 
     /* TODO: We have to find a way to select the location of counters dynamically. Will 
-       distributing counters across processes in a round-robin fashion be a good way? */
+     distributing counters across processes in a round-robin fashion be a good way? */
     a1d_counter->rank = A1D_Process_info.num_ranks - 1;
 
     if (a1d_counter->rank == A1D_Process_info.my_rank)
@@ -122,14 +121,14 @@ int A1D_Create_counter(A1_group_t* group, A1_counter_t *counter_ptr)
 
         for (index = 0; index < A1D_Process_info.num_ranks; index++)
         {
-            if(index != A1D_Process_info.my_rank)
-            { 
+            if (index != A1D_Process_info.my_rank)
+            {
                 status = DCMF_Control(&A1D_Counter_create_protocol,
                                       DCMF_SEQUENTIAL_CONSISTENCY,
                                       index,
                                       &cmsg);
                 A1U_ERR_POP(status != DCMF_SUCCESS,
-                        "DCMF_Control failed in A1D_Alloc_counter\n");
+                            "DCMF_Control failed in A1D_Alloc_counter\n");
             }
         }
 
@@ -143,13 +142,11 @@ int A1D_Create_counter(A1_group_t* group, A1_counter_t *counter_ptr)
         a1d_counter->value_ptr = counter_create_response;
     }
 
-  fn_exit:
-    A1DI_CRITICAL_EXIT();
+    fn_exit: A1DI_CRITICAL_EXIT();
     A1U_FUNC_EXIT();
     return status;
 
-  fn_fail:
-    goto fn_exit;
+    fn_fail: goto fn_exit;
 }
 
 int A1D_Destroy_counter(A1_group_t* group, A1_counter_t *counter_ptr)
@@ -163,51 +160,44 @@ int A1D_Destroy_counter(A1_group_t* group, A1_counter_t *counter_ptr)
     A1DI_CRITICAL_ENTER();
 
     A1U_ERR_POP(status = (group != A1_GROUP_WORLD),
-            "Counters are not implemented for non-world groups!");
+                "Counters are not implemented for non-world groups!");
 
     A1DI_GlobalBarrier();
 
     a1d_counter = *counter_ptr;
 
     A1DI_Free(a1d_counter);
-    
+
     *counter_ptr = NULL;
 
-  fn_exit:
-    A1DI_CRITICAL_EXIT();
+    fn_exit: A1DI_CRITICAL_EXIT();
     A1U_FUNC_EXIT();
     return status;
 
-  fn_fail:
-    goto fn_exit;
+    fn_fail: goto fn_exit;
 }
 
-int A1D_Incr_counter(A1_counter_t counter,
-                     long increment,
-                     long* original)
+int A1D_Incr_counter(A1_counter_t counter, long increment, long* original)
 {
     int status = A1_SUCCESS;
     A1D_Counter_t *a1d_counter;
     A1D_Counter_pkt_t counter_pkt;
     DCMF_Control_t cmsg;
-    
+
     A1U_FUNC_ENTER();
 
     A1DI_CRITICAL_ENTER();
-  
-    a1d_counter = (A1D_Counter_t *) counter; 
- 
-    if(a1d_counter->rank == A1D_Process_info.my_rank)
+
+    a1d_counter = (A1D_Counter_t *) counter;
+
+    if (a1d_counter->rank == A1D_Process_info.my_rank)
     {
-
         *original = a1d_counter->value;
-
         a1d_counter->value = a1d_counter->value + increment;
-
     }
     else
     {
-        
+
         counter_pkt.value_ptr = a1d_counter->value_ptr;
         counter_pkt.value = increment;
 
@@ -220,7 +210,7 @@ int A1D_Incr_counter(A1_counter_t counter,
                               a1d_counter->rank,
                               &cmsg);
         A1U_ERR_POP(status != DCMF_SUCCESS,
-                   "DCMF_Control failed in A1D_Incr_counter\n");
+                    "DCMF_Control failed in A1D_Incr_counter\n");
 
         A1DI_Conditional_advance(counter_incr_active > 0);
 
@@ -228,12 +218,10 @@ int A1D_Incr_counter(A1_counter_t counter,
 
     }
 
-  fn_exit:
-    A1DI_CRITICAL_EXIT();
+    fn_exit: A1DI_CRITICAL_EXIT();
     A1U_FUNC_EXIT();
     return status;
 
-  fn_fail:
-    goto fn_exit;
+    fn_fail: goto fn_exit;
 }
 
