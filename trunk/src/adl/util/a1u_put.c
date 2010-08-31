8e8346ff37365b20930383e53c17688697cfd8a5
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-31 18:22:52 +0000

    still unimplemented
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@338 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/adl/util/a1u_put.c
+++ trunk/src/adl/util/a1u_put.c
@@ -8,7 +8,92 @@
 #include "a1u.h"
 #include "a1d.h"
 
-int A1U_Put_memcpy(int target, void* src, void* dst, int bytes)
+int A1U_Put_memcpy(int target,
+                   void* src,
+                   void* dst,
+                   int bytes)
+{
+    int status = A1_SUCCESS;
+
+    A1U_FUNC_ENTER();
+
+    fn_exit: A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail: goto fn_exit;
+}
+
+int A1U_NbPut_memcpy(int target,
+                     void* src,
+                     void* dst,
+                     int bytes,
+                     A1_handle_t a1_handle)
+{
+    int status = A1_SUCCESS;
+
+    A1U_FUNC_ENTER();
+
+    fn_exit: A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail: goto fn_exit;
+}
+
+int A1U_PutS_memcpy(int target,
+                    int stride_level,
+                    int *block_sizes,
+                    void* source_ptr,
+                    int *src_stride_ar,
+                    void* target_ptr,
+                    int *trg_stride_ar)
+{
+    int status = A1_SUCCESS;
+
+    A1U_FUNC_ENTER();
+
+    fn_exit: A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail: goto fn_exit;
+}
+
+int A1U_NbPutS_memcpy(int target,
+                      int stride_level,
+                      int *block_sizes,
+                      void* source_ptr,
+                      int *src_stride_ar,
+                      void* target_ptr,
+                      int *trg_stride_ar,
+                      A1_handle_t a1_handle)
+{
+    int status = A1_SUCCESS;
+
+    A1U_FUNC_ENTER();
+
+    fn_exit: A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail: goto fn_exit;
+}
+
+int A1U_PutV_memcpy(int target,
+                    A1_iov_t *iov_ar,
+                    int ar_len)
+{
+    int status = A1_SUCCESS;
+
+    A1U_FUNC_ENTER();
+
+    fn_exit: A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail: goto fn_exit;
+}
+
+int A1U_NbPutV_memcpy(int target,
+                      A1_iov_t *iov_ar,
+                      int ar_len,
+                      A1_handle_t a1_handle)
 {
     int status = A1_SUCCESS;
 
