5437b752d1f59a5ce0e543314393f7cec2aeac63
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-08-31 19:34:05 +0000

    still unimplemented for the most part
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@344 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/adl/util/a1u_acc.c
+++ trunk/src/adl/util/a1u_acc.c
@@ -7,3 +7,53 @@
 #include "a1.h"
 #include "a1u.h"
 #include "a1d.h"
+
+int A1U_Acc_memcpy(void* source_ptr,
+                   void* target_ptr,
+                   int bytes,
+                   A1_datatype_t a1_type,
+                   void* scaling)
+{
+    int status = A1_SUCCESS;
+
+    A1U_FUNC_ENTER();
+
+    fn_exit: A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail: goto fn_exit;
+}
+
+int A1U_AccS_memcpy(int stride_level,
+                    int *block_sizes,
+                    void* source_ptr,
+                    int *src_stride_ar,
+                    void* target_ptr,
+                    int *trg_stride_ar,
+                    A1_datatype_t a1_type,
+                    void* scaling)
+{
+    int status = A1_SUCCESS;
+
+    A1U_FUNC_ENTER();
+
+    fn_exit: A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail: goto fn_exit;
+}
+
+int A1U_AccV_memcpy(A1_iov_t *iov_ar,
+                    int ar_len,
+                    A1_datatype_t a1_type,
+                    void* scaling)
+{
+    int status = A1_SUCCESS;
+
+    A1U_FUNC_ENTER();
+
+    fn_exit: A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail: goto fn_exit;
+}
--- trunk/src/adl/util/a1u_put.c
+++ trunk/src/adl/util/a1u_put.c
@@ -8,8 +8,7 @@
 #include "a1u.h"
 #include "a1d.h"
 
-int A1U_Put_memcpy(int target,
-                   void* src,
+int A1U_Put_memcpy(void* src,
                    void* dst,
                    int bytes)
 {
@@ -17,21 +16,7 @@ int A1U_Put_memcpy(int target,
 
     A1U_FUNC_ENTER();
 
-    fn_exit: A1U_FUNC_EXIT();
-    return status;
-
-    fn_fail: goto fn_exit;
-}
-
-int A1U_NbPut_memcpy(int target,
-                     void* src,
-                     void* dst,
-                     int bytes,
-                     A1_handle_t a1_handle)
-{
-    int status = A1_SUCCESS;
-
-    A1U_FUNC_ENTER();
+    A1DI_Memcpy(dst, src, bytes);
 
     fn_exit: A1U_FUNC_EXIT();
     return status;
@@ -39,8 +24,7 @@ int A1U_NbPut_memcpy(int target,
     fn_fail: goto fn_exit;
 }
 
-int A1U_PutS_memcpy(int target,
-                    int stride_level,
+int A1U_PutS_memcpy(int stride_level,
                     int *block_sizes,
                     void* source_ptr,
                     int *src_stride_ar,
@@ -57,27 +41,7 @@ int A1U_PutS_memcpy(int target,
     fn_fail: goto fn_exit;
 }
 
-int A1U_NbPutS_memcpy(int target,
-                      int stride_level,
-                      int *block_sizes,
-                      void* source_ptr,
-                      int *src_stride_ar,
-                      void* target_ptr,
-                      int *trg_stride_ar,
-                      A1_handle_t a1_handle)
-{
-    int status = A1_SUCCESS;
-
-    A1U_FUNC_ENTER();
-
-    fn_exit: A1U_FUNC_EXIT();
-    return status;
-
-    fn_fail: goto fn_exit;
-}
-
-int A1U_PutV_memcpy(int target,
-                    A1_iov_t *iov_ar,
+int A1U_PutV_memcpy(A1_iov_t *iov_ar,
                     int ar_len)
 {
     int status = A1_SUCCESS;
@@ -90,18 +54,4 @@ int A1U_PutV_memcpy(int target,
     fn_fail: goto fn_exit;
 }
 
-int A1U_NbPutV_memcpy(int target,
-                      A1_iov_t *iov_ar,
-                      int ar_len,
-                      A1_handle_t a1_handle)
-{
-    int status = A1_SUCCESS;
-
-    A1U_FUNC_ENTER();
-
-    fn_exit: A1U_FUNC_EXIT();
-    return status;
-
-    fn_fail: goto fn_exit;
-}
 
