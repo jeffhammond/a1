172c4c2da9d4e31aa3e3631320094c7967faba99
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-09-17 02:48:00 +0000

    this is working pretty nicely now.  ready to deal with un-alignment and integrate into DCMFD.
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@414 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/benchmarks/bgp/accumulate.c
+++ trunk/benchmarks/bgp/accumulate.c
@@ -23,96 +23,84 @@ static __inline__ unsigned long long getticks(void)
 
 int main(int argc, char* argv[])
 {
-    int i,j,k,count=10;
+    fprintf(stderr,"BEGIN TESTING OF DP ACCUMULATE\n");
 
-    int dim = ( argc>1 ? atoi(argv[1]) : 1024 );
-    printf("TESTING DP ACCUMULATE OF DIMENSION %d\n",dim);
+    int k;
 
-    unsigned long long t0, t1;
-
-    double* a;
-    double* b;
-    double* c;
-
-    double  scale = 0.1;
-
-    posix_memalign((void**)&a, 16*sizeof(double), dim*sizeof(double));
-    posix_memalign((void**)&b, 16*sizeof(double), dim*sizeof(double));
-    posix_memalign((void**)&c, 16*sizeof(double), dim*sizeof(double));
-
-    for (i=0;i<dim;i++) a[i] = 1.0 - 2*(double)rand()/(double)RAND_MAX;
-
-    printf("BASIC VERSION\n");
-
-    // WARM-UP
-    for (i=0;i<dim;i++) b[i] = 0.0;
-    for (i=0;i<dim;i++) b[i] += scale*a[i];
-
-    // TIMING
-    for (i=0;i<dim;i++) b[i] = 0.0;
-    t0 = getticks();
-    for (j=0;j<count;j++)
+    for (k=6;k<20;k++)
     {
+        int dim = pow(2,k);
+
+        int count = 10;
+
+        int i,j;
+
+        unsigned long long t0, t1;
+        unsigned long long dt0, dt1, dt2, dt3, dt4;
+
+        double* a;
+        double* b;
+        double* c;
+
+        double  scale = 0.1;
+
+        posix_memalign((void**)&a, 16*sizeof(double), dim*sizeof(double));
+        posix_memalign((void**)&b, 16*sizeof(double), dim*sizeof(double));
+        posix_memalign((void**)&c, 16*sizeof(double), dim*sizeof(double));
+
+        for (i=0;i<dim;i++) a[i] = 1.0 - 2*(double)rand()/(double)RAND_MAX;
+
+        fprintf(stderr,"BASIC VERSION\n");
+
+        // WARM-UP
+        for (i=0;i<dim;i++) b[i] = 0.0;
         for (i=0;i<dim;i++) b[i] += scale*a[i];
-    }
-    t1 = getticks();
-    printf("time for basic version        = %30llu\n",t1-t0);
 
-    printf("INTRINSICS VERSION 1\n");
+        // TIMING
+        for (i=0;i<dim;i++) b[i] = 0.0;
+        t0 = getticks();
+        for (j=0;j<count;j++)
+        {
+            for (i=0;i<dim;i++) b[i] += scale*a[i];
+        }
+        t1 = getticks();
+        dt0 = t1 - t0;
 
-    // WARM-UP
-    for (i=0;i<dim;i++) c[i] = 0.0;
-    for (i=0;i<dim;i+=2)
-    {
-        __stfpd(&c[i], __fxcpmadd( __lfpd(&c[i]), __lfpd(&a[i]), scale) );
-    }
+        fprintf(stderr,"INTRINSICS VERSION 1\n");
 
-    // TIMING
-    for (i=0;i<dim;i++) c[i] = 0.0;
-    t0 = getticks();
-    for (j=0;j<count;j++)
-    {
+        // WARM-UP
+        for (i=0;i<dim;i++) c[i] = 0.0;
         for (i=0;i<dim;i+=2)
         {
             __stfpd(&c[i], __fxcpmadd( __lfpd(&c[i]), __lfpd(&a[i]), scale) );
         }
-    }
-    t1 = getticks();
-    printf("time for intrinsics version 1 = %30llu\n",t1-t0);
 
-    // VERIFICATION
-    for (i=0;i<dim;i++)
-    {
-        if (b[i] != c[i])
+        // TIMING
+        for (i=0;i<dim;i++) c[i] = 0.0;
+        t0 = getticks();
+        for (j=0;j<count;j++)
         {
-            printf("%4d %30.15f %30.15f\n",i,b[i],c[i]);
+            for (i=0;i<dim;i+=2)
+            {
+                __stfpd(&c[i], __fxcpmadd( __lfpd(&c[i]), __lfpd(&a[i]), scale) );
+            }
         }
-    }
+        t1 = getticks();
+        dt1 = t1 - t0;
 
-    printf("INTRINSICS VERSION 2\n");
-
-    // WARM-UP
-    for (i=0;i<dim;i++) c[i] = 0.0;
-    for (i=0;i<dim;i+=4)
-    {
+        // VERIFICATION
+        for (i=0;i<dim;i++)
         {
-            double _Complex a0, a2, c0, c2;
-            a0 = __lfpd(&a[i  ]);
-            a2 = __lfpd(&a[i+2]);
-            c0 = __lfpd(&c[i  ]);
-            c2 = __lfpd(&c[i+2]);
-            c0 = __fxcpmadd(c0,a0,scale);
-            c2 = __fxcpmadd(c2,a2,scale);
-            __stfpd(&c[i  ],c0);
-            __stfpd(&c[i+2],c2);
+            if (b[i] != c[i])
+            {
+                printf("%4d %30.15f %30.15f\n",i,b[i],c[i]);
+            }
         }
-    }
 
-    // TIMING
-    for (i=0;i<dim;i++) c[i] = 0.0;
-    t0 = getticks();
-    for (j=0;j<count;j++)
-    {
+        fprintf(stderr,"INTRINSICS VERSION 2\n");
+
+        // WARM-UP
+        for (i=0;i<dim;i++) c[i] = 0.0;
         for (i=0;i<dim;i+=4)
         {
             {
@@ -127,122 +115,219 @@ int main(int argc, char* argv[])
                 __stfpd(&c[i+2],c2);
             }
         }
-    }
-    t1 = getticks();
-    printf("time for intrinsics version 2 = %30llu\n",t1-t0);
 
-    // VERIFICATION
-    for (i=0;i<dim;i++)
-    {
-        if (b[i] != c[i])
+        // TIMING
+        for (i=0;i<dim;i++) c[i] = 0.0;
+        t0 = getticks();
+        for (j=0;j<count;j++)
         {
-            printf("%4d %30.15f %30.15f\n",i,b[i],c[i]);
+            for (i=0;i<dim;i+=4)
+            {
+                {
+                    double _Complex a0, a2, c0, c2;
+                    a0 = __lfpd(&a[i  ]);
+                    a2 = __lfpd(&a[i+2]);
+                    c0 = __lfpd(&c[i  ]);
+                    c2 = __lfpd(&c[i+2]);
+                    c0 = __fxcpmadd(c0,a0,scale);
+                    c2 = __fxcpmadd(c2,a2,scale);
+                    __stfpd(&c[i  ],c0);
+                    __stfpd(&c[i+2],c2);
+                }
+            }
         }
-    }
+        t1 = getticks();
+        dt2 = t1 - t0;
 
-    printf("INTRINSICS VERSION 3\n");
+        // VERIFICATION
+        for (i=0;i<dim;i++)
+        {
+            if (b[i] != c[i])
+            {
+                printf("%4d %30.15f %30.15f\n",i,b[i],c[i]);
+            }
+        }
 
-    // WARM-UP
-    for (i=0;i<dim;i++) c[i] = 0.0;
-    for (i=0;i<dim;i+=8)
-    {
-        __stfpd(&c[i  ], __fxcpmadd( __lfpd(&c[i  ]), __lfpd(&a[i  ]), scale) );
-        __stfpd(&c[i+2], __fxcpmadd( __lfpd(&c[i+2]), __lfpd(&a[i+2]), scale) );
-        __stfpd(&c[i+4], __fxcpmadd( __lfpd(&c[i+4]), __lfpd(&a[i+4]), scale) );
-        __stfpd(&c[i+6], __fxcpmadd( __lfpd(&c[i+6]), __lfpd(&a[i+6]), scale) );
-    }
+        fprintf(stderr,"INTRINSICS VERSION 3\n");
 
-    // TIMING
-    for (i=0;i<dim;i++) c[i] = 0.0;
-    t0 = getticks();
-    for (j=0;j<count;j++)
-    {
+        // WARM-UP
+        for (i=0;i<dim;i++) c[i] = 0.0;
         for (i=0;i<dim;i+=8)
         {
-            __stfpd(&c[i  ], __fxcpmadd( __lfpd(&c[i  ]), __lfpd(&a[i  ]), scale) );
-            __stfpd(&c[i+2], __fxcpmadd( __lfpd(&c[i+2]), __lfpd(&a[i+2]), scale) );
-            __stfpd(&c[i+4], __fxcpmadd( __lfpd(&c[i+4]), __lfpd(&a[i+4]), scale) );
-            __stfpd(&c[i+6], __fxcpmadd( __lfpd(&c[i+6]), __lfpd(&a[i+6]), scale) );
+            {
+                double _Complex a0, a2, a4, a6;
+                double _Complex c0, c2, c4, c6;
+                a0 = __lfpd(&a[i  ]);
+                a2 = __lfpd(&a[i+2]);
+                a4 = __lfpd(&a[i+4]);
+                a6 = __lfpd(&a[i+6]);
+                c0 = __lfpd(&c[i  ]);
+                c2 = __lfpd(&c[i+2]);
+                c4 = __lfpd(&c[i+4]);
+                c6 = __lfpd(&c[i+6]);
+                c0 = __fxcpmadd(c0,a0,scale);
+                c2 = __fxcpmadd(c2,a2,scale);
+                c4 = __fxcpmadd(c4,a4,scale);
+                c6 = __fxcpmadd(c6,a6,scale);
+                __stfpd(&c[i  ],c0);
+                __stfpd(&c[i+2],c2);
+                __stfpd(&c[i+4],c4);
+                __stfpd(&c[i+6],c6);
+            }
         }
-    }
-    t1 = getticks();
-    printf("time for intrinsics version 3 = %30llu\n",t1-t0);
 
-    // VERIFICATION
-    for (i=0;i<dim;i++)
-    {
-        if (b[i] != c[i])
+        // TIMING
+        for (i=0;i<dim;i++) c[i] = 0.0;
+        t0 = getticks();
+        for (j=0;j<count;j++)
         {
-            printf("%4d %30.15f %30.15f\n",i,b[i],c[i]);
+            for (i=0;i<dim;i+=8)
+            {
+                {
+                    double _Complex a0, a2, a4, a6;
+                    double _Complex c0, c2, c4, c6;
+                    a0 = __lfpd(&a[i  ]);
+                    a2 = __lfpd(&a[i+2]);
+                    a4 = __lfpd(&a[i+4]);
+                    a6 = __lfpd(&a[i+6]);
+                    c0 = __lfpd(&c[i  ]);
+                    c2 = __lfpd(&c[i+2]);
+                    c4 = __lfpd(&c[i+4]);
+                    c6 = __lfpd(&c[i+6]);
+                    c0 = __fxcpmadd(c0,a0,scale);
+                    c2 = __fxcpmadd(c2,a2,scale);
+                    c4 = __fxcpmadd(c4,a4,scale);
+                    c6 = __fxcpmadd(c6,a6,scale);
+                    __stfpd(&c[i  ],c0);
+                    __stfpd(&c[i+2],c2);
+                    __stfpd(&c[i+4],c4);
+                    __stfpd(&c[i+6],c6);
+                }
+            }
         }
-    }
+        t1 = getticks();
+        dt3 = t1 - t0;
 
-    printf("INTRINSICS VERSION 4\n");
+        // VERIFICATION
+        for (i=0;i<dim;i++)
+        {
+            if (b[i] != c[i])
+            {
+                printf("%4d %30.15f %30.15f\n",i,b[i],c[i]);
+            }
+        }
 
-    // WARM-UP
-    for (i=0;i<dim;i++) c[i] = 0.0;
-    for (i=0;i<dim;i+=16)
-    {
-        __stfpd(&c[i   ], __fxcpmadd( __lfpd(&c[i   ]), __lfpd(&a[i   ]), scale) );
-        __stfpd(&c[i+ 2], __fxcpmadd( __lfpd(&c[i+ 2]), __lfpd(&a[i+ 2]), scale) );
-        __stfpd(&c[i+ 4], __fxcpmadd( __lfpd(&c[i+ 4]), __lfpd(&a[i+ 4]), scale) );
-        __stfpd(&c[i+ 6], __fxcpmadd( __lfpd(&c[i+ 6]), __lfpd(&a[i+ 6]), scale) );
-        __stfpd(&c[i+ 8], __fxcpmadd( __lfpd(&c[i+ 8]), __lfpd(&a[i+ 8]), scale) );
-        __stfpd(&c[i+10], __fxcpmadd( __lfpd(&c[i+10]), __lfpd(&a[i+10]), scale) );
-        __stfpd(&c[i+12], __fxcpmadd( __lfpd(&c[i+12]), __lfpd(&a[i+12]), scale) );
-        __stfpd(&c[i+14], __fxcpmadd( __lfpd(&c[i+14]), __lfpd(&a[i+14]), scale) );
-    }
+        fprintf(stderr,"INTRINSICS VERSION 4\n");
 
-    // TIMING
-    for (i=0;i<dim;i++) c[i] = 0.0;
-    t0 = getticks();
-    for (j=0;j<count;j++)
-    {
+        // WARM-UP
+        for (i=0;i<dim;i++) c[i] = 0.0;
         for (i=0;i<dim;i+=16)
         {
-            __stfpd(&c[i   ], __fxcpmadd( __lfpd(&c[i   ]), __lfpd(&a[i   ]), scale) );
-            __stfpd(&c[i+ 2], __fxcpmadd( __lfpd(&c[i+ 2]), __lfpd(&a[i+ 2]), scale) );
-            __stfpd(&c[i+ 4], __fxcpmadd( __lfpd(&c[i+ 4]), __lfpd(&a[i+ 4]), scale) );
-            __stfpd(&c[i+ 6], __fxcpmadd( __lfpd(&c[i+ 6]), __lfpd(&a[i+ 6]), scale) );
-            __stfpd(&c[i+ 8], __fxcpmadd( __lfpd(&c[i+ 8]), __lfpd(&a[i+ 8]), scale) );
-            __stfpd(&c[i+10], __fxcpmadd( __lfpd(&c[i+10]), __lfpd(&a[i+10]), scale) );
-            __stfpd(&c[i+12], __fxcpmadd( __lfpd(&c[i+12]), __lfpd(&a[i+12]), scale) );
-            __stfpd(&c[i+14], __fxcpmadd( __lfpd(&c[i+14]), __lfpd(&a[i+14]), scale) );
+            {
+                double _Complex a0, a2, a4, a6, a8, a10, a12, a14;
+                double _Complex c0, c2, c4, c6, c8, c10, c12, c14;
+                a0 = __lfpd(&a[i   ]);
+                a2 = __lfpd(&a[i+ 2]);
+                a4 = __lfpd(&a[i+ 4]);
+                a6 = __lfpd(&a[i+ 6]);
+                a4 = __lfpd(&a[i+ 8]);
+                a6 = __lfpd(&a[i+10]);
+                a4 = __lfpd(&a[i+12]);
+                a6 = __lfpd(&a[i+14]);
+                c0 = __lfpd(&c[i   ]);
+                c2 = __lfpd(&c[i+ 2]);
+                c4 = __lfpd(&c[i+ 4]);
+                c6 = __lfpd(&c[i+ 6]);
+                c4 = __lfpd(&c[i+ 8]);
+                c6 = __lfpd(&c[i+10]);
+                c4 = __lfpd(&c[i+12]);
+                c6 = __lfpd(&c[i+14]);
+                c0 = __fxcpmadd( c0, a0,scale);
+                c2 = __fxcpmadd( c2, a2,scale);
+                c4 = __fxcpmadd( c4, a4,scale);
+                c6 = __fxcpmadd( c6, a6,scale);
+                c4 = __fxcpmadd( c8, a8,scale);
+                c6 = __fxcpmadd(c10,a10,scale);
+                c4 = __fxcpmadd(c12,a12,scale);
+                c6 = __fxcpmadd(c14,a14,scale);
+                __stfpd(&c[i   ],c0);
+                __stfpd(&c[i+ 2],c2);
+                __stfpd(&c[i+ 4],c4);
+                __stfpd(&c[i+ 6],c6);
+                __stfpd(&c[i+ 8],c4);
+                __stfpd(&c[i+10],c6);
+                __stfpd(&c[i+12],c4);
+                __stfpd(&c[i+14],c6);
+            }
         }
-    }
-    t1 = getticks();
-    printf("time for intrinsics version 4 = %30llu\n",t1-t0);
 
-    // VERIFICATION
-    for (i=0;i<dim;i++)
-    {
-        if (b[i] != c[i])
+        // TIMING
+        for (i=0;i<dim;i++) c[i] = 0.0;
+        t0 = getticks();
+        for (j=0;j<count;j++)
         {
-            printf("%4d %30.15f %30.15f\n",i,b[i],c[i]);
+            for (i=0;i<dim;i+=16)
+            {
+                {
+                    double _Complex a0, a2, a4, a6, a8, a10, a12, a14;
+                    double _Complex c0, c2, c4, c6, c8, c10, c12, c14;
+                    a0  = __lfpd(&a[i   ]);
+                    a2  = __lfpd(&a[i+ 2]);
+                    a4  = __lfpd(&a[i+ 4]);
+                    a6  = __lfpd(&a[i+ 6]);
+                    a8  = __lfpd(&a[i+ 8]);
+                    a10 = __lfpd(&a[i+10]);
+                    a12 = __lfpd(&a[i+12]);
+                    a14 = __lfpd(&a[i+14]);
+                    c0  = __lfpd(&c[i   ]);
+                    c2  = __lfpd(&c[i+ 2]);
+                    c4  = __lfpd(&c[i+ 4]);
+                    c6  = __lfpd(&c[i+ 6]);
+                    c8  = __lfpd(&c[i+ 8]);
+                    c10 = __lfpd(&c[i+10]);
+                    c12 = __lfpd(&c[i+12]);
+                    c14 = __lfpd(&c[i+14]);
+                    c0  = __fxcpmadd( c0, a0,scale);
+                    c2  = __fxcpmadd( c2, a2,scale);
+                    c4  = __fxcpmadd( c4, a4,scale);
+                    c6  = __fxcpmadd( c6, a6,scale);
+                    c8  = __fxcpmadd( c8, a8,scale);
+                    c10 = __fxcpmadd(c10,a10,scale);
+                    c12 = __fxcpmadd(c12,a12,scale);
+                    c14 = __fxcpmadd(c14,a14,scale);
+                    __stfpd(&c[i   ], c0);
+                    __stfpd(&c[i+ 2], c2);
+                    __stfpd(&c[i+ 4], c4);
+                    __stfpd(&c[i+ 6], c6);
+                    __stfpd(&c[i+ 8], c8);
+                    __stfpd(&c[i+10],c10);
+                    __stfpd(&c[i+12],c12);
+                    __stfpd(&c[i+14],c14);
+                }
+            }
         }
+        t1 = getticks();
+        dt4 = t1 - t0;
+
+        // VERIFICATION
+        for (i=0;i<dim;i++)
+        {
+            if (b[i] != c[i])
+            {
+                printf("%4d %30.15f %30.15f\n",i,b[i],c[i]);
+            }
+        }
+
+        if (k==6) printf("%18s %18s %18s %18s %18s %18s\n","dim","basic","1-hummer","2-hummers","4-hummers","8-hummers");
+        printf("%18d %18llu %18llu %18llu %18llu %18llu\n",dim,dt0,dt1,dt2,dt3,dt4);
+
+        free(a);
+        free(b);
+        free(c);
+
     }
 
-    printf("BASIC VERSION\n");
-
-    // WARM-UP
-    for (i=0;i<dim;i++) b[i] = 0.0;
-    for (i=0;i<dim;i++) b[i] += scale*a[i];
-
-    // TIMING
-    for (i=0;i<dim;i++) b[i] = 0.0;
-    t0 = getticks();
-    for (j=0;j<count;j++)
-    {
-        for (i=0;i<dim;i++) b[i] += scale*a[i];
-    }
-    t1 = getticks();
-    printf("time for basic version        = %30llu\n",t1-t0);
-
-    free(a);
-    free(b);
-    free(c);
-
-    printf("ALL DONE\n");
+    fprintf(stderr,"ALL DONE\n");
 
     return(0);
 }
