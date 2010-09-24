84917e5b3169e3153e566add7434eb6c469bacde
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-09-24 02:14:34 +0000

    Print_parameters moved to ~after~ my_rank is set so that printing can be limited to process 0 only
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@501 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/a1d/dcmfd/dcmfd_initialize.c
+++ trunk/src/a1d/dcmfd/dcmfd_initialize.c
@@ -39,10 +39,6 @@ int A1D_Initialize(int thread_level)
     A1U_ERR_POP(status != A1_SUCCESS,
                 "A1DI_Read_parameters returned with error \n");
 
-    status = A1DI_Print_parameters();
-    A1U_ERR_POP(status != A1_SUCCESS,
-                "A1DI_Print_parameters returned with error \n");
-
     if (a1d_settings.enable_cht)
     {
         if (!a1d_settings.mpi_active)
@@ -109,6 +105,10 @@ int A1D_Initialize(int thread_level)
     }
 
     A1DI_CRITICAL_ENTER();
+
+    status = A1DI_Print_parameters();
+    A1U_ERR_POP(status != A1_SUCCESS,
+                "A1DI_Print_parameters returned with error \n");
 
     status = A1DI_Control_xchange_initialize();
     A1U_ERR_POP(status != A1_SUCCESS,
