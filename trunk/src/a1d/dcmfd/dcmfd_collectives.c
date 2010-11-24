f65942ea72e5ebe42906988427c93d3c970de4f2
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-11-24 20:08:52 +0000

    reorg
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@665 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/a1d/dcmfd/dcmfd_collectives.c
+++ trunk/src/a1d/dcmfd/dcmfd_collectives.c
@@ -73,72 +73,6 @@ int A1DI_GlobalBarrier()
 
 }
 
-int A1D_Barrier_group(A1_group_t* group)
-{
-    int status = A1_SUCCESS;
-
-    A1U_FUNC_ENTER();
-
-    A1DI_CRITICAL_ENTER();
-
-    if (group == A1_GROUP_WORLD || group == NULL)
-    {
-
-        status = A1DI_GlobalBarrier();
-        A1U_ERR_ABORT(status != A1_SUCCESS,
-                      "DCMF_GlobalBarrier returned with an error");
-        goto fn_exit;
-    }
-    else
-    {
-        A1U_ERR_POP(1,
-                    "A1D_Barrier_group not implemented for non-world groups!");
-        goto fn_fail;
-    }
-
-    fn_exit: A1DI_CRITICAL_EXIT();
-    A1U_FUNC_EXIT();
-    return status;
-
-    fn_fail: goto fn_exit;
-
-}
-
-int A1D_Sync_group(A1_group_t* group)
-{
-
-    int status = A1_SUCCESS;
-
-    A1U_FUNC_ENTER();
-
-    A1DI_CRITICAL_ENTER();
-
-    if (group == A1_GROUP_WORLD || group == NULL)
-    {
-        status = A1DI_Flush_all();
-        A1U_ERR_ABORT(status != A1_SUCCESS,
-                      "A1DI_Flush_all returned with an error");
-        status = A1DI_GlobalBarrier();
-        A1U_ERR_ABORT(status != A1_SUCCESS,
-                      "A1DI_GlobalBarrier returned with an error");
-        goto fn_exit;
-    }
-    else
-    {
-        A1U_ERR_POP(1, "A1D_Sync_group not implemented for non-world groups!");
-        goto fn_fail;
-    }
-
-    fn_exit:
-    A1DI_CRITICAL_EXIT();
-    A1U_FUNC_EXIT();
-    return status;
-
-    fn_fail:
-    goto fn_exit;
-
-}
-
 int A1DI_NbGlobalBarrier(A1D_Handle_t *a1d_handle)
 {
 
@@ -166,82 +100,6 @@ int A1DI_NbGlobalBarrier(A1D_Handle_t *a1d_handle)
                   "DCMF_GlobalBarrier returned with an error");
 
     fn_exit:
-    A1U_FUNC_EXIT();
-    return status;
-
-    fn_fail:
-    goto fn_exit;
-
-}
-
-int A1D_NbBarrier_group(A1_group_t* group, A1_handle_t a1_handle)
-{
-    int status = A1_SUCCESS;
-    A1D_Handle_t *a1d_handle;
-
-    A1U_FUNC_ENTER();
-
-    A1DI_CRITICAL_ENTER();
-
-    if (group == A1_GROUP_WORLD || group == NULL)
-    {
-        a1d_handle = (A1D_Handle_t *) a1_handle;
-
-        status = A1DI_NbGlobalBarrier(a1d_handle);
-        A1U_ERR_ABORT(status != A1_SUCCESS,
-                      "DCMF_NbGlobalBarrier returned with an error");
-
-        goto fn_exit;
-    }
-    else
-    {
-        A1U_ERR_POP(1,
-                    "A1D_NbBarrier_group not implemented for non-world groups!");
-        goto fn_fail;
-    }
-
-    fn_exit:
-    A1DI_CRITICAL_EXIT();
-    A1U_FUNC_EXIT();
-    return status;
-
-    fn_fail:
-    goto fn_exit;
-
-}
-
-int A1D_NbSync_group(A1_group_t* group, A1_handle_t a1_handle)
-{
-    int status = A1_SUCCESS;
-    A1D_Handle_t *a1d_handle;
-
-    A1U_FUNC_ENTER();
-
-    A1DI_CRITICAL_ENTER();
-
-    if (group == A1_GROUP_WORLD || group == NULL)
-    {
-        a1d_handle = (A1D_Handle_t *) a1_handle;
-
-        /*This has to be replace with a non-blocking flushall to make it truly non blocking*/
-        status = A1DI_Flush_all();
-        A1U_ERR_ABORT(status != A1_SUCCESS,
-                      "A1DI_Flush_all returned with an error");
-
-        status = A1DI_NbGlobalBarrier(a1d_handle);
-        A1U_ERR_ABORT(status != A1_SUCCESS,
-                      "A1DI_NbGlobalBarrier returned with an error");
-
-        goto fn_exit;
-    }
-    else
-    {
-        A1U_ERR_POP(1, "A1D_NbSync_group not implemented for non-world groups!");
-        goto fn_fail;
-    }
-
-    fn_exit:
-    A1DI_CRITICAL_EXIT();
     A1U_FUNC_EXIT();
     return status;
 
@@ -449,6 +307,247 @@ int A1DI_GlobalAllreduce(int count,
 
     fn_fail:
     goto fn_exit;
+}
+
+int A1DI_GlobalBcast_initialize()
+{
+    int status = A1_SUCCESS;
+    DCMF_GlobalBcast_Configuration_t conf;
+
+    A1U_FUNC_ENTER();
+
+    conf.protocol = DCMF_DEFAULT_GLOBALBCAST_PROTOCOL;
+    status = DCMF_GlobalBcast_register(&A1D_GlobalBcast_protocol,
+                                       &conf);
+    A1U_ERR_POP(status != DCMF_SUCCESS,
+                "DCMF_GlobalBcast_register returned with error %d \n",
+                status);
+
+    fn_exit:
+    A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail:
+    goto fn_exit;
+}
+
+int A1DI_GlobalBcast(int root,
+                     int count,
+                     void *buffer)
+{
+    int status = A1_SUCCESS;
+    DCMF_Request_t request;
+    DCMF_Callback_t done_callback;
+    volatile unsigned gb_active = 0;
+
+    A1U_FUNC_ENTER();
+
+    gb_active += 1;
+    done_callback.function = A1DI_Generic_done;
+    done_callback.clientdata = (void *) &gb_active;
+
+    status = DCMF_GlobalBcast(&A1D_GlobalBcast_protocol,
+                              &request,
+                              done_callback,
+                              DCMF_SEQUENTIAL_CONSISTENCY,
+                              root,
+                              (char *) buffer,
+                              count);
+    A1U_ERR_POP(status != DCMF_SUCCESS,
+                "DCMF_GlobalBcast returned with error %d \n",
+                status);
+
+    A1DI_Conditional_advance(gb_active > 0);
+
+    fn_exit:
+    A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail:
+    goto fn_exit;
+}
+
+int A1DI_NbGlobalBcast(int root,
+                       int count,
+                       void *buffer,
+                       A1D_Handle_t *a1d_handle)
+{
+    int status = A1_SUCCESS;
+    A1D_Request_t *a1d_request;
+    DCMF_Callback_t done_callback;
+
+    A1U_FUNC_ENTER();
+
+    a1d_request = A1DI_Get_request(1);
+    A1U_ERR_POP(status = (a1d_request == NULL),
+                "A1DI_Get_request returned error \n");
+    A1DI_Set_handle(a1d_request, a1d_handle);
+
+    a1d_handle->active++;
+
+    done_callback.function = A1DI_Request_done;
+    done_callback.clientdata = (void *) a1d_request;
+
+    status = DCMF_GlobalBcast(&A1D_GlobalBcast_protocol,
+                              &(a1d_request->request),
+                              done_callback,
+                              DCMF_SEQUENTIAL_CONSISTENCY,
+                              root,
+                              (char *) buffer,
+                              count);
+    A1U_ERR_POP(status != DCMF_SUCCESS,
+                "DCMF_GlobalBcast returned with error %d \n",
+                status);
+
+    fn_exit:
+    A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail:
+    goto fn_exit;
+}
+
+/********************************************************/
+
+int A1D_Barrier_group(A1_group_t* group)
+{
+    int status = A1_SUCCESS;
+
+    A1U_FUNC_ENTER();
+
+    A1DI_CRITICAL_ENTER();
+
+    if (group == A1_GROUP_WORLD || group == NULL)
+    {
+
+        status = A1DI_GlobalBarrier();
+        A1U_ERR_ABORT(status != A1_SUCCESS,
+                      "DCMF_GlobalBarrier returned with an error");
+        goto fn_exit;
+    }
+    else
+    {
+        A1U_ERR_POP(1,
+                    "A1D_Barrier_group not implemented for non-world groups!");
+        goto fn_fail;
+    }
+
+    fn_exit: A1DI_CRITICAL_EXIT();
+    A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail: goto fn_exit;
+
+}
+
+int A1D_NbBarrier_group(A1_group_t* group, A1_handle_t a1_handle)
+{
+    int status = A1_SUCCESS;
+    A1D_Handle_t *a1d_handle;
+
+    A1U_FUNC_ENTER();
+
+    A1DI_CRITICAL_ENTER();
+
+    if (group == A1_GROUP_WORLD || group == NULL)
+    {
+        a1d_handle = (A1D_Handle_t *) a1_handle;
+
+        status = A1DI_NbGlobalBarrier(a1d_handle);
+        A1U_ERR_ABORT(status != A1_SUCCESS,
+                      "DCMF_NbGlobalBarrier returned with an error");
+
+        goto fn_exit;
+    }
+    else
+    {
+        A1U_ERR_POP(1,
+                    "A1D_NbBarrier_group not implemented for non-world groups!");
+        goto fn_fail;
+    }
+
+    fn_exit:
+    A1DI_CRITICAL_EXIT();
+    A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail:
+    goto fn_exit;
+
+}
+
+int A1D_Sync_group(A1_group_t* group)
+{
+
+    int status = A1_SUCCESS;
+
+    A1U_FUNC_ENTER();
+
+    A1DI_CRITICAL_ENTER();
+
+    if (group == A1_GROUP_WORLD || group == NULL)
+    {
+        status = A1DI_Flush_all();
+        A1U_ERR_ABORT(status != A1_SUCCESS,
+                      "A1DI_Flush_all returned with an error");
+        status = A1DI_GlobalBarrier();
+        A1U_ERR_ABORT(status != A1_SUCCESS,
+                      "A1DI_GlobalBarrier returned with an error");
+        goto fn_exit;
+    }
+    else
+    {
+        A1U_ERR_POP(1, "A1D_Sync_group not implemented for non-world groups!");
+        goto fn_fail;
+    }
+
+    fn_exit:
+    A1DI_CRITICAL_EXIT();
+    A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail:
+    goto fn_exit;
+
+}
+
+int A1D_NbSync_group(A1_group_t* group, A1_handle_t a1_handle)
+{
+    int status = A1_SUCCESS;
+    A1D_Handle_t *a1d_handle;
+
+    A1U_FUNC_ENTER();
+
+    A1DI_CRITICAL_ENTER();
+
+    if (group == A1_GROUP_WORLD || group == NULL)
+    {
+        a1d_handle = (A1D_Handle_t *) a1_handle;
+
+        /*This has to be replace with a non-blocking flushall to make it truly non blocking*/
+        status = A1DI_Flush_all();
+        A1U_ERR_ABORT(status != A1_SUCCESS,
+                      "A1DI_Flush_all returned with an error");
+
+        status = A1DI_NbGlobalBarrier(a1d_handle);
+        A1U_ERR_ABORT(status != A1_SUCCESS,
+                      "A1DI_NbGlobalBarrier returned with an error");
+
+        goto fn_exit;
+    }
+    else
+    {
+        A1U_ERR_POP(1, "A1D_NbSync_group not implemented for non-world groups!");
+        goto fn_fail;
+    }
+
+    fn_exit:
+    A1DI_CRITICAL_EXIT();
+    A1U_FUNC_EXIT();
+    return status;
+
+    fn_fail:
+    goto fn_exit;
 
 }
 
@@ -523,64 +622,7 @@ int A1D_NbAllreduce_group(A1_group_t* group,
 
 }
 
-int A1DI_GlobalBcast_initialize()
-{
-    int status = A1_SUCCESS;
-    DCMF_GlobalBcast_Configuration_t conf;
 
-    A1U_FUNC_ENTER();
-
-    conf.protocol = DCMF_DEFAULT_GLOBALBCAST_PROTOCOL;
-    status = DCMF_GlobalBcast_register(&A1D_GlobalBcast_protocol,
-                                       &conf);
-    A1U_ERR_POP(status != DCMF_SUCCESS,
-                "DCMF_GlobalBcast_register returned with error %d \n",
-                status);
-
-    fn_exit:
-    A1U_FUNC_EXIT();
-    return status;
-
-    fn_fail:
-    goto fn_exit;
-}
-
-int A1DI_GlobalBcast(int root,
-                     int count,
-                     void *buffer)
-{
-    int status = A1_SUCCESS;
-    DCMF_Request_t request;
-    DCMF_Callback_t done_callback;
-    volatile unsigned gb_active = 0;
-
-    A1U_FUNC_ENTER();
-
-    gb_active += 1;
-    done_callback.function = A1DI_Generic_done;
-    done_callback.clientdata = (void *) &gb_active;
-
-    status = DCMF_GlobalBcast(&A1D_GlobalBcast_protocol,
-                              &request,
-                              done_callback,
-                              DCMF_SEQUENTIAL_CONSISTENCY,
-                              root,
-                              (char *) buffer,
-                              count);
-    A1U_ERR_POP(status != DCMF_SUCCESS,
-                "DCMF_GlobalBcast returned with error %d \n",
-                status);
-
-    A1DI_Conditional_advance(gb_active > 0);
-
-    fn_exit:
-    A1U_FUNC_EXIT();
-    return status;
-
-    fn_fail:
-    goto fn_exit;
-
-}
 
 int A1D_Bcast_group(A1_group_t* group,
                     int root,
@@ -613,49 +655,8 @@ int A1D_Bcast_group(A1_group_t* group,
     return status;
 
     fn_fail: goto fn_exit;
-
 }
 
-int A1DI_NbGlobalBcast(int root,
-                       int count,
-                       void *buffer,
-                       A1D_Handle_t *a1d_handle)
-{
-    int status = A1_SUCCESS;
-    A1D_Request_t *a1d_request;
-    DCMF_Callback_t done_callback;
-
-    A1U_FUNC_ENTER();
-
-    a1d_request = A1DI_Get_request(1);
-    A1U_ERR_POP(status = (a1d_request == NULL),
-                "A1DI_Get_request returned error \n");
-    A1DI_Set_handle(a1d_request, a1d_handle);
-
-    a1d_handle->active++;
-
-    done_callback.function = A1DI_Request_done;
-    done_callback.clientdata = (void *) a1d_request;
-
-    status = DCMF_GlobalBcast(&A1D_GlobalBcast_protocol,
-                              &(a1d_request->request),
-                              done_callback,
-                              DCMF_SEQUENTIAL_CONSISTENCY,
-                              root,
-                              (char *) buffer,
-                              count);
-    A1U_ERR_POP(status != DCMF_SUCCESS,
-                "DCMF_GlobalBcast returned with error %d \n",
-                status);
-
-    fn_exit:
-    A1U_FUNC_EXIT();
-    return status;
-
-    fn_fail:
-    goto fn_exit;
-
-}
 
 int A1D_NbBcast_group(A1_group_t* group,
                       int root,
