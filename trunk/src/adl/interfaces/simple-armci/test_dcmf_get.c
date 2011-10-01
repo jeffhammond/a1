175362f115d87485e5c635905d38a0db16c2f4a4
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2011-10-01 15:58:35 +0000

    add query
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@803 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/adl/interfaces/simple-armci/test_dcmf_get.c
+++ trunk/src/adl/interfaces/simple-armci/test_dcmf_get.c
@@ -1,53 +1,3 @@
-/*
- * The following is a notice of limited availability of the code, and disclaimer
- * which must be included in the prologue of the code and in all source listings
- * of the code.
- *
- * Copyright (c) 2010  Argonne Leadership Computing Facility, Argonne National
- * Laboratory
- *
- * Permission is hereby granted to use, reproduce, prepare derivative works, and
- * to redistribute to others.
- *
- *
- *                          LICENSE
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- * - Redistributions of source code must retain the above copyright
- *   notice, this list of conditions and the following disclaimer.
- *
- * - Redistributions in binary form must reproduce the above copyright
- *   notice, this list of conditions and the following disclaimer listed
- *   in this license in the documentation and/or other materials
- *   provided with the distribution.
- *
- * - Neither the name of the copyright holders nor the names of its
- *   contributors may be used to endorse or promote products derived from
- *   this software without specific prior written permission.
- *
- * The copyright holders provide no reassurances that the source code
- * provided does not infringe any patent, copyright, or any other
- * intellectual property rights of third parties.  The copyright holders
- * disclaim any liability to any recipient for claims brought against
- * recipient by any third party for infringement of that parties
- * intellectual property rights.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -108,6 +58,9 @@ int main(int argc, char *argv[])
         DCMF_Memregion_t * memregion_list = (DCMF_Memregion_t *) malloc( size * sizeof(DCMF_Memregion_t) );
         assert( memregion_list!=NULL );
 
+        void ** baseptr_list = (void *) malloc( size * sizeof(void *) );
+        assert( baseptr_list!=NULL );
+
         mpi_status = MPI_Barrier(MPI_COMM_WORLD);
         assert(mpi_status==0);
 
@@ -115,6 +68,14 @@ int main(int argc, char *argv[])
                                    memregion_list,    sizeof(DCMF_Memregion_t), MPI_BYTE,
                                    MPI_COMM_WORLD);
         assert(mpi_status==0);
+
+        DCMF_CriticalSection_enter(0);
+        for (int i=0; i<size; i++)
+        {
+            dcmf_result = DCMF_Memregion_query( &memregion_list[i], &bytes_out, &baseptr_list[i] );
+            assert( dcmf_result==DCMF_SUCCESS );
+        }
+        DCMF_CriticalSection_exit(0);
 
         mpi_status = MPI_Barrier(MPI_COMM_WORLD);
         assert(mpi_status==0);
@@ -153,13 +114,13 @@ int main(int argc, char *argv[])
                     dcmf_result = DCMF_Get(&get_protocol,
                                            &request,
                                            done_callback,
-                                           DCMF_RELAXED_CONSISTENCY,
-                                           //DCMF_SEQUENTIAL_CONSISTENCY,
+                                           //DCMF_RELAXED_CONSISTENCY,
+                                           DCMF_SEQUENTIAL_CONSISTENCY,
                                            target,
                                            bytes,
                                            &memregion_list[target],
                                            &local_memregion,
-                                           0,
+                                           baseptr_list,
                                            0);
 
                     while (active > 0) DCMF_Messager_advance();
