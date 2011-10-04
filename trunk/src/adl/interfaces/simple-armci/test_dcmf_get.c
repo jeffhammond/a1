bf0270902660053f88d72150db54633f079ed44c
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2011-10-04 22:53:18 +0000

    oops
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@812 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/adl/interfaces/simple-armci/test_dcmf_get.c
+++ trunk/src/adl/interfaces/simple-armci/test_dcmf_get.c
@@ -209,4 +209,9 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+#else
+int main()
+{
+    return(1);
+}
 #endif
--- trunk/src/adl/interfaces/simple-armci/test_dcmf_get_bw.c
+++ trunk/src/adl/interfaces/simple-armci/test_dcmf_get_bw.c
@@ -163,4 +163,9 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+#else
+int main()
+{
+    return(1);
+}
 #endif
