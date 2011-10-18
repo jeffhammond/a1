0c03071b7f49090f10b2b212d4391e2a7555e1a7
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2011-10-18 22:29:07 +0000

    very basic test of DCMF_Send
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@843 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/adl/interfaces/simple-armci/test_dcmf_send.c
+++ trunk/src/adl/interfaces/simple-armci/test_dcmf_send.c
@@ -7,49 +7,204 @@
 #include <mpi.h>
 #include <dcmf.h>
 
-DCMF_Protocol_t default_proto;
-DCMF_Protocol_t eager_proto;
-DCMF_Protocol_t rzv_proto;
-
 static int rank = -1, size = -1;
 
-void cb_done(void * clientdata, DCMF_Error_t * error)
-{
-    printf("%d: cb_done \n", rank);
+/***************************************************************/
 
-    --(*((uint32_t *) clientdata));
+static int              recv_peer;
+static DCMF_Request_t * recv_request;
+static void *           recv_buffer;
+static volatile int    local_active;
+static volatile int    remote_active;
+
+/***************************************************************/
+
+void local_completion_cb(void * clientdata, const DCMF_Control_t * info, size_t peer)
+{
+    printf("local_completion_cb rank=%d peer=%d \n", rank, peer);
+
+    local_active = 0;
+
+    return;
+}
+
+/***************************************************************/
+
+DCMF_Protocol_t control_proto;
+
+void remote_completion_cb(void * clientdata, const DCMF_Control_t * info, size_t peer)
+{
+    printf("remote_completion_cb rank=%d peer=%d \n", rank, peer);
+
+    remote_active = 0;
+
+    return;
+}
+
+/***************************************************************/
+
+void default_long_cleanup_cb(void * clientdata, DCMF_Error_t * error)
+{
+    int peer = (int) clientdata;
+
+    printf("default_long_cleanup_cb rank=%d peer=%d \n", rank, peer);
+
+    free(recv_request);
+    free(recv_buffer);
+
+    dcmf_result =  DCMF_Control(&remote_completion_cb,
+                                DCMF_SEQUENTIAL_CONSISTENCY,
+                                peer,
+                                NULL);
+    assert(dcmf_result==DCMF_SUCCESS);
+
+    return;
+}
+
+void default_short_cb(void *clientdata,
+                      const DCQuad *msginfo,
+                      unsigned count,
+                      size_t peer,
+                      const char *src,
+                      size_t bytes)
+{
+    size_t i;
+
+    printf("default_short_cb rank=%d peer=%d count=%u \n", rank, peer, count);
+    for (i=0;i<bytes;i++)
+        printf("%d: src[%d] = %c \n", rank, i, src[i]);
+
+    return;
+}
+
+DCMF_Request_t * default_long_cb(void *clientdata,
+                                 const DCQuad *msginfo,
+                                 unsigned count,
+                                 size_t peer,
+                                 size_t sndlen,
+                                 size_t *rcvlen,
+                                 char **rcvbuf,
+                                 DCMF_Callback_t *cb_done)
+{
+    int rc = 0;
+    printf("default_long_cb rank=%d peer=%d count=%u \n", rank, peer, count);
+
+    rc = posix_memalign( &recv_request, 128, DCMF_Request_t );
+    assert( (rc == 0) && (recv_request != NULL) );
+
+    rc = posix_memalign( &recv_buffer, 128, sndlen );
+    assert( (rc == 0) && (recv_buffer != NULL) );
+
+    (*rcvlen) = sndlen;
+    (*rcvbuf) = &recv_buffer;
+
+    cb_done->function   = default_long_cleanup_cb;
+    cb_done->clientdata = (void *) peer;
+
+    return recv_request;
 }
 
 int main(int argc, char *argv[])
 {
-    int provided;
+    int rc;
     int mpi_status;
     DCMF_Result dcmf_result;
 
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    /***************************************************************/
+
+    int provided;
+    MPI_Init_thread( &argc, &argv, MPI_THREAD_MULTIPLE, &provided );
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+    MPI_Comm_size( MPI_COMM_WORLD, &size );
     assert( size > 1 );
 
-    DCMF_Send_Configuration_t default_conf;
-    DCMF_Send_Configuration_t eager_conf;
-    DCMF_Send_Configuration_t rzv_conf;
+    /***************************************************************/
 
-    default_conf.protocol = DCMF_DEFAULT_SEND_PROTOCOL;
-    default_conf.network = DCMF_DEFAULT_NETWORK;
+    DCMF_Control_Configuration_t control_conf;
 
-    dcmf_result = DCMF_Get_register(&get_protocol, &conf);
-    assert(dcmf_result==DCMF_SUCCESS);
-
-    mpi_status = MPI_Barrier(MPI_COMM_WORLD);
-    assert(mpi_status==0);
+    control_conf.protocol           = DCMF_DEFAULT_CONTROL_PROTOCOL;
+    control_conf.network            = DCMF_DEFAULT_NETWORK;
+    control_conf.cb_recv            = remote_completion_cb;
+    control_conf.cb_recv_clientdata = NULL;
 
     DCMF_CriticalSection_enter(0);
+    dcmf_result = DCMF_Control_register(&control_proto, &control_conf);
     DCMF_CriticalSection_exit(0);
     assert(dcmf_result==DCMF_SUCCESS);
 
+    /***************************************************************/
+
+    DCMF_Protocol_t default_proto;
+    DCMF_Send_Configuration_t default_conf;
+
+    default_conf.protocol                 = DCMF_DEFAULT_SEND_PROTOCOL;
+    default_conf.network                  = DCMF_DEFAULT_NETWORK;
+    default_conf.cb_recv_short            = default_short_cb;
+    default_conf.cb_recv_short_clientdata = NULL;
+    default_conf.cb_recv                  = default_long_cb;
+    default_conf.cb_recv_clientdata       = NULL;
+
+    DCMF_CriticalSection_enter(0);
+    dcmf_result = DCMF_Send_register(&default_proto, &default_conf);
+    DCMF_CriticalSection_exit(0);
+    assert(dcmf_result==DCMF_SUCCESS);
+
+    /***************************************************************/
+
     mpi_status = MPI_Barrier(MPI_COMM_WORLD);
     assert(mpi_status==0);
+
+    /***************************************************************/
+
+    if ( rank == 0 )
+    {
+        int i;
+        DCMF_Request_t request,
+        size_t target = 1;
+        size_t send_bytes = ( argc > 1 ? atoi(argv[1]) : 1 );
+        char * send_buffer = NULL;
+
+        rc = posix_memalign( &send_buffer, 128, send_bytes );
+        assert( (rc == 0) && (send_buffer != NULL) );
+
+        for (i=0;i<bytes;i++) send_buffer[i] = 'X';
+
+        local_active  = 1;
+        remote_active = 1;
+
+        printf("before DCMF_Send \n");
+
+        DCMF_CriticalSection_enter(0);
+        status = DCMF_Send(&default_proto,
+                           &request,
+                           local_completion_cb,
+                           DCMF_SEQUENTIAL_CONSISTENCY,
+                           target,
+                           bytes,
+                           send_buffer,
+                           NULL,
+                           0);
+        assert(dcmf_result==DCMF_SUCCESS);
+
+        printf("after DCMF_Send \n");
+
+        while ( local_active ) DCMF_Messager_advance(0);
+
+        printf("after local completion \n");
+
+        while ( remote_active ) DCMF_Messager_advance(0);
+
+        printf("after remote completion \n");
+
+        DCMF_CriticalSection_exit(0);
+    }
+
+    /***************************************************************/
+
+    mpi_status = MPI_Barrier(MPI_COMM_WORLD);
+    assert(mpi_status==0);
+
+    /***************************************************************/
 
     MPI_Finalize();
 
