5b500557a91ddd2b2dfc619e688837f1db4e16dd
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2011-11-13 00:42:15 +0000

    this is the smallest possible ARMCI wrapper to A1 that enables SCAFACOS to work
    git-svn-id: https://svn.mcs.anl.gov/repos/a1@999 4a0c8edc-5189-0410-a5da-cd986011a6a8

---
+++ branches/scafacos/armci.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include <mpi.h>
+
+int ARMCI_Init();
+void ARMCI_Finalize();
+void ARMCI_Cleanup();
+void ARMCI_Error(char *msg, int code);
+
+int ARMCI_Malloc(void * ptr_arr[], int bytes);
+int ARMCI_Free(void * ptr);
+
+void ARMCI_Barrier();
+
+void ARMCI_Fence(int proc);
+void ARMCI_AllFence();
+
+int ARMCI_Put(void *src, void *dst, int bytes, int proc);
